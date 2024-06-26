3dbdd8c3ed87ab439de5781542291d2a783bbc25
dsartori <doug@sartori.ca>
2024-06-26 10:34:49 -0400

    poi copy
    update POIs
    update tour sequence

--- src/theme.ts
+++ src/theme.ts
@@ -1,10 +1,10 @@
 export const theme = {
   colors: {
-    primary: "#7246AF",
-    secondary: "#F8F2F7",
-    tertiary: "#F2CEEB",
-    quaternary: "#CECFDF",
-    quinary: "#6870CD",
+    primary: "#044B7F",
+    secondary: "#FFFFFA",
+    tertiary: "#E3B505",
+    quaternary: "#E3B505",
+    quinary: "#E3B505",
     senary: "#DDD9E8",
     septenary: "#CCC",
     octonary: "#000",
